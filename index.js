99f44bac05005fa73dbf34e592497e1e6bfa7c85
Scale-Up SaaS <100524415+saasscaleup@users.noreply.github.com>
2022-03-31 16:37:01 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,70 @@
+// Load aws sdk
+var aws = require('aws-sdk');
+
+// Load aws Rekognition object
+var rekognition = new aws.Rekognition();
+
+// Set aws lambda handler function
+exports.handler = (event, context, callback) => {
+  
+    // Buffer created to receive the encode base64 image
+    var buffer = new Buffer.from(event.imageFormat, 'base64');
+    
+    // aws Rekognition required params
+    var params = {
+        Image :{
+            Bytes: buffer
+        },
+        MaxLabels: 10, // Max labels we are allowing to get back. Can be changed
+        MinConfidence: 75 // // Min labels detect A.I. Confidance level. 1%-100%
+    }; 
+  
+   //send params to Amazon rekognition
+    rekognition.detectLabels(params, function(error, data){
+      
+        //if there is an error, return the description about it
+        if(error){
+          
+            // Prepare error response
+            var ErrorOcurred = {
+              "fileName": event.fileName,
+              "Error":{
+                "Error": error.stack,
+                "Description": error
+              }
+            };
+            
+            // invoke callback function with error response
+            callback(null, ErrorOcurred);
+        }
+      
+        //if everything is ok, send the labels of the image
+        else{
+            
+            // Prepare success response
+            var res = {
+                "fileName": event.fileName,
+                "Tags": data
+            };
+          
+            // invoke callback function with image labels response
+            callback(null, res);
+        }
+    });
+};
+
+// Success response example
+/*
+   data = {
+    Labels: [
+       {
+      Confidence: 99.25072479248047, 
+      Name: "People"
+     }, 
+       {
+      Confidence: 99.25074005126953, 
+      Name: "Person"
+     }
+    ]
+   }
+*/
